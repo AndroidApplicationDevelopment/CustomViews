9d20ff59730980c4140854412e58d674421520a0
Gravener <gravener@BAM-024170-RG.(none)>
2013-06-04 19:26:28 -0400

    use paint

--- src/us/wmwm/customviews/MainActivity.java
+++ src/us/wmwm/customviews/MainActivity.java
@@ -1,15 +1,19 @@
 package us.wmwm.customviews;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	ResizingTextView textView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 	}
 
 	@Override
--- src/us/wmwm/customviews/ResizingTextView.java
+++ src/us/wmwm/customviews/ResizingTextView.java
@@ -1,30 +1,70 @@
 package us.wmwm.customviews;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class ResizingTextView extends View {
 
 	String txt;
 	
-	public ResizingTextView(Context context) {
-		this(context,null);
-	}
+	Paint paint;
+	
 
 	public ResizingTextView(Context context, AttributeSet attrs) {
-		super(context, attrs);
+		this(context, attrs,0);
+		
+		
 	}
 	
+	
+	
+	public ResizingTextView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		
+		final TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ResizingTextView, defStyle, 0);
+		
+		txt = a.getString(R.styleable.ResizingTextView_text);
+		
+		a.recycle();
+		Log.d("ResizingTextView", txt);
+		
+		paint = new Paint();
+		
+		paint.setColor(Color.BLACK);
+		
+	}
+
+
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		
+		canvas.drawText(txt, 0, 0, paint);
+		
 	}
 	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		
+//		final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+//        final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+//        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+//        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        
+        float width = paint.measureText(txt);      
+        
+        Log.d("onMeasure", width+"");
+        
+        //setMeasuredDimension(getMeasuredWidth(), );
+		
 	}
 	
 	
